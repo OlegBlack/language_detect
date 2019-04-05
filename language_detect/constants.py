fd50de1de2250d223d2ab7a3331ee79b68630903
Oleg B <demon_cs@ukr.net>
2019-04-05 22:59:33 +0300

    upload language_detect

---
+++ language_detect/constants.py
@@ -0,0 +1,81 @@
+ITALIAN_BOCCACCIO_TEXT = """Mossi adunque piú cosí egregi come antichi popoli da questa laudevole
+sentenzia e apertissimamente vera, alcuna volta di deitá, altra di
+marmorea statua, e sovente di celebre sepultura, e tal fiata di
+triunfale arco, e quando di laurea corona secondo i meriti precedenti
+onoravano i valorosi: le pene, per opposito, a' colpevoli date non
+curo di raccontare. Per li quali onori e purgazioni la assiria, la
+macedonica, la greca e ultimamente la romana republica aumentate, con
+
+l'opere le fini della terra, e con la fama toccaron le stelle. Le
+vestigie de' quali in cosí alti esempli, non solamente da' successori
+presenti, e massimamente da' miei fiorentini, sono male seguite, ma in
+tanto s'è disviato da esse, che ogni premio di virtú possiede
+l'ambizione; per che, sí come e io e ciascun altro che a ciò con
+occhio ragionevole vuole guardare, non senza grandissima afflizione
+d'animo possiamo vedere li malvagi e perversi uomini a' luoghi eccelsi
+e a' sommi ofici e guiderdoni elevare, e li buoni scacciare, deprimere
+e abbassare. Alle quali cose qual fine serbi il giudicio di Dio,
+coloro il veggiano che il timone governano di questa nave: percioché
+noi, piú bassa turba, siamo trasportati dal fiotto, della fortuna, ma
+non della colpa partecipi. E, comeché con infinite ingratitudini e
+dissolute perdonanze apparenti si potessero le predette cose
+verificare, per meno scoprire li nostri difetti e per pervenire al mio
+principale intento, una sola mi fia assai avere raccontata (né questa
+fia poco o picciola), ricordando l'esilio del chiarissimo uomo Dante
+Alighieri. Il quale, antico cittadino né d'oscuri parenti nato, quanto
+per vertú e per scienzia e per buone operazioni meritasse, assai il
+mostrano e mostreranno le cose che da lui fatte appaiono: le quali, se
+in una republica giusta fossero state operate, niuno dubbio ci è che
+esse non gli avessero altissimi meriti apparecchiati."""
+
+DUTCH_ETHERLANDS_TEXT = """Zijn vader had vier kinderen, drie zoons en één dochter. Hij moet een
+verdienstelijk en verstandig man zijn geweest, want hij schijnt aan al
+zijne kinderen het onderwijs te hebben doen geven, dat de gewone school
+aanbood. Christophorus had goed leeren lezen, schrijven en rekenen. Ook
+had hij eenige vorderingen gemaakt in het Latijn en het teekenen. Zelfs
+bezocht hij de hoogeschool te Pavia, waar hij zich vlijtig oefende
+in meetkunde, aardrijkskunde, sterrekunde en zeevaartkunde."""
+
+FINNISH_KIVI_TEXT = """Jukolan talo, eteläisessä Hämeessä, seisoo erään mäen pohjaisella
+rinteellä, liki Toukolan kylää. Sen läheisin ympäristö on kivinen
+tanner, mutta alempana alkaa pellot, joissa, ennenkuin talo oli häviöön
+mennyt, aaltoili teräinen vilja. Peltojen alla on niittu, apilaäyräinen,
+halkileikkaama monipolvisen ojan; ja runsaasti antoi se heiniä,
+ennenkuin joutui laitumeksi kylän karjalle. Muutoin on talolla avaria
+metsiä, soita ja erämaita, jotka, tämän tilustan ensimmäisen perustajan
+oivallisen toiminnan kautta, olivat langenneet sille osaksi jo ison-jaon
+käydessä entisinä aikoina. Silloinpa Jukolan isäntä, pitäen enemmän
+huolta jälkeentulevainsa edusta kuin omasta parhaastansa, otti vastaan
+osaksensa kulon polttaman metsän ja sai sillä keinolla seitsemän vertaa
+enemmän kuin toiset naapurinsa. Mutta kaikki kulovalkean jäljet olivat
+jo kadonneet hänen piiristänsä ja tuuhea metsä kasvanut sijaan.--Ja tämä
+on niiden seitsemän veljen koto, joiden elämänvaiheita tässä nyt käyn
+kertoilemaan."""
+
+CZECH_TEXT = """Nejvíce používaným dopravním prostředkem je dnes bezpochyby auto.
+ Jízda autem je rychlá a pohodlná. Auto nás dopraví k cíli bez dlouhých hodin čekání.
+  V posledních letech se dopravní síť značně zlepšila. Bylo vybudováno mnoho nových 
+  cest, dálnic, mostů a kolejí, které přispěly k tomu, že se můžeme dostat rychleji k 
+  našemu cíli. S rozvojem silnic a dálnic vzrostl také počet dopravních prostředků na 
+  našich cestách. Mnohdy jsou cesty přeplněny osobními a nákladními auty, autobusy,
+   motocykly a na silnicích také cyklisty. Každý účastník silničního provozu se musí
+    podrobit pravidlům bezpečné jízdy. Ale přesto jsme často svědky mnoha dopravních nehod.
+Ne každý z nás si může dovolit auto, a proto jezdí mnoho lidí také jinými dopravními 
+prostředky. Nejrychlejším dopravním prosředkem, ale zároveň také nejdražším, je letadlo.
+ Před odletem je nutné obstarat si letenku a na letišti se včas podrobit pasové a celní 
+ kontrole. Potom už jen čekáme na náš let. Někdy musí být odlet kvůli nepříznivým povětrnostním
+  podmínkám odložen, potom musíme čekat v odletové hale. Na palubě letadla nás obsluhují letušky,
+   které nabízejí malé občerstvení, noviny nebo videofilmy."""
+
+# Define variables
+# csv separator
+CSV_SEP = ';'
+CSV_FILE = 'letter_frequency.csv'
+
+STATUS_SUCCES = 0
+STATUS_FAIL = -1
+STATUS_EXIT = -1
+
+PROMPT = 'Please enter file: '
+
+PICKLE_FILE = 'model.pikle'
